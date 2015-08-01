29bd3873b123abe400207300964a5c6cdf5116b8
JiangangLu <jgangl123@163.com>
2015-08-01 21:47:57 +0800

    update gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,41 @@ var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
+var templateCache = require('gulp-angular-templatecache');
+var clean = require('gulp-clean');
+
+//for minify js
+var uglify = require('gulp-uglify');
+var ngmin = require('gulp-ngmin');
+var stripDebug = require('gulp-strip-debug');
 
 var paths = {
-  sass: ['./scss/**/*.scss']
+  sass: ['./scss/**/*.scss'],
+  templatecache: ['./www/templates/**/*.html'],
+  minifyjs: ['./www/js/**/*.js','!./www/js/dist/main.min.js']
 };
 
-gulp.task('default', ['sass']);
+//主任务
+gulp.task('default', ['clean', 'sass', 'templatecache', 'minifyjs']);
 
+//clean
+gulp.task('clean', function() {
+    return gulp.src(['./www/js/dist/main.min.js'], {read: false})
+        .pipe(clean({force: true}));
+});
+
+//js
+gulp.task('minifyjs', function() {
+    return gulp.src(['./www/js/**/*.js','!./www/js/dist/main.min.js'])
+        .pipe(ngmin({dynamic: false}))
+        // .pipe(stripDebug())
+        .pipe(uglify({outSourceMap: false}))
+        .pipe(concat('main.min.js'))
+        .pipe(gulp.dest('./www/js/dist'))
+});
+
+
+//sass
 gulp.task('sass', function(done) {
   gulp.src('./scss/ionic.app.scss')
     .pipe(sass({
@@ -27,8 +55,25 @@ gulp.task('sass', function(done) {
     .on('end', done);
 });
 
+//html template
+gulp.task('templatecache', function (done) {
+    gulp.src('./www/templates/**/*.html')
+        .pipe(templateCache({
+            standalone:true,
+            base: function(file) {
+              // var filename = /[^/]*$/.exec( file.relative )[0];
+              // return 'templates/' + filename;
+              return 'templates/'+ file.relative;
+        }}))
+        .pipe(gulp.dest('./www/js/dist'))
+        .on('end', done);
+});
+
+//watch the change
 gulp.task('watch', function() {
   gulp.watch(paths.sass, ['sass']);
+  gulp.watch(paths.templatecache, ['templatecache']);
+  gulp.watch(paths.minifyjs, ['minifyjs']);
 });
 
 gulp.task('install', ['git-check'], function() {
